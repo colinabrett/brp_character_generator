41aff2e8b9cd55c6607ce32e58543d139d51c90f
Patrick Maslen <patrick@quest-bird.com>
2016-08-06 20:27:53 +1000

    derived attributes now working in GameSystem -PM

--- game_system.py
+++ game_system.py
@@ -4,13 +4,14 @@
 #
 from dice_roller import *
 from brp_stats import *
+from decimal import Decimal, ROUND_HALF_UP
 
 class GameSystem():
         def __init__(self):
                 self.statblock = {}
                 self.derived = {}
                 self.skills = {}
-                self.supressed_skills = {}
+                self.supressed_skills = []
                 self.supressed_stats = {}
                 self.bonuses = {}
 
@@ -18,7 +19,10 @@ class GameSystem():
                 """modify STATS if they exist in the statblock eg. statslist {'STR':'2d6', 'EDU':'2d8'}"""
                 for attribute, value in statslist.items():
                         if attribute in self.statblock:
-                                self.statblock[attribute] = straight_dice(*parseDiceString(value))
+                                if not isinstance(value, int):
+                                        self.statblock[attribute] = straight_dice(*parseDiceString(value))
+                                else:
+                                        self.statblock[attribute] = value
                 return
 
         def calculateDerived(self) :
@@ -37,8 +41,12 @@ class GameSystem():
                 """remove a skill from the base list for a particular genre"""
                 return
 
-        def supressStat(self, skill):
+        def supressStat(self, stat):
                 """remove a stat from the base list for a particular genre eg. no EDU in a fantasy game"""
+                # for a stat to be added it should be in the statblock and not in the suppressed_stats list
+                ustat = stat.upper()
+                if ustat in self.statblock and ustat not in self.supressed_stats:
+                        self.supressed_stats.append(ustat)
                 return
 
 class Brp(GameSystem):
@@ -121,6 +129,7 @@ class Brp(GameSystem):
                                'Technical Skill Common' :  5,\
                                'Throw' : 25,\
                                'Track' : 10 }
+                self.supressed_stats = []
                 self.bonuses = {}
 
         def calculateBonuses(self):
@@ -129,10 +138,13 @@ class Brp(GameSystem):
                 'Combat' : {'primary' : ['DEX'], 'secondary' : ['INT','STR'], 'negative' : []},\
                 'Communication' : {'primary' : ['INT'], 'secondary' : ['POW','APP'], 'negative' : []},\
                 'Manipulation': {'primary' : ['DEX'], 'secondary' : ['INT','STR'], 'negative' : []},\
-                'Mental': {'primary' : ['INT'], 'secondary' : ['POW','EDU'], 'negative' : []},\
+                'Mental': {'primary' : ['INT'], 'secondary' : ['POW'], 'negative' : []},\
                 'Perception': {'primary' : ['INT'], 'secondary' : ['POW','CON'], 'negative' : []},\
                 'Physical': {'primary' : ['DEX'], 'secondary' : ['STR','CON'], 'negative' : ['SIZ']}
                 }
+                if 'EDU' not in self.supressed_stats:
+                        skill_groups['Mental']['secondary'].append('EDU')
+                        
                 for category,modifiers in skill_groups.items():
                         bonus = 0
                         primary = modifiers['primary']
@@ -145,10 +157,23 @@ class Brp(GameSystem):
                         for stat in negative:
                                 bonus -= calculate_primary_bonus(self.statblock[stat])
                         self.bonuses[category] = bonus
-
-     
-        
-                                
+                        
+        def calculateDerived(self):
+                """calculate rolls and stats derived from attributes"""
+                self.derived = {
+                        'Damage Bonus': damage_bonus(self.statblock['STR'], self.statblock['SIZ']),\
+                        'Hit Points' : int(Decimal((self.statblock['CON'] + self.statblock['SIZ'])/2).quantize(0, ROUND_HALF_UP)),\
+                        'Experience Bonus' : int(Decimal(self.statblock['INT']/2).quantize(0, ROUND_HALF_UP)),\
+                        'Effort Roll' : self.statblock['STR'] * 5,\
+                        'Stamina Roll' : self.statblock['CON'] * 5,\
+                        'Idea Roll' : self.statblock['INT'] * 5,\
+                        'Luck Roll' : self.statblock['POW'] * 5,\
+                        'Agility Roll' : self.statblock['DEX'] *5,\
+                        'Charisma Roll' : self.statblock['APP'] *5\
+                        }
+                self.derived['Major Wound level'] = int(Decimal(self.derived['Hit Points']/2).quantize(0, ROUND_HALF_UP))
+                if 'EDU' not in self.supressed_stats:
+                                self.derived['Know Roll'] = self.statblock['EDU'] * 5
 
     
     
