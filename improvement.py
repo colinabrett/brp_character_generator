e2c2ad42f0fb8ec2a3c380b2aad73c670581d505
Patrick Maslen <patrick@quest-bird.com>
2016-08-22 13:04:47 +1000

    added ProfessionImprovementMW, which takes tuples of (no. of skills, points) instead of straight points PM

--- improvement.py
+++ improvement.py
@@ -7,8 +7,13 @@
 # Profession might do more for example
 #
 # A GameSystem takes a list of these things.
+# need another type which takes maybe a list of tuples of skills, points
+# a la Magic World. Like ProfessionImprovement; maybe a subclass
 from dice_roller import *
 from random import shuffle
+from collections import Iterable
+def typecheck(obj):
+    return not isinstance(obj, str) and isinstance(obj, Iterable)
 
 class Improvement():
     def __init__(self, skills_dict):
@@ -19,6 +24,7 @@ class Improvement():
         return self.skills
 
 class ProfessionImprovement(Improvement):
+    """This one takes a number of skill points and allocates them in some way to the skills dictionary"""
     def __init__(self, skills_dict):
         self.skills = skills_dict
         self.points = 0
@@ -65,6 +71,50 @@ class ProfessionImprovement(Improvement):
                 break   
         while skill_names:
             self.categorised_skills['Other'].append(skill_names.pop())
-        
-        
+            
+class ProfesssionImprovementMW(Improvement):
+    """As ProfessionImprovement, except that the allocation method is by tuples of (number of skills, points to allocate), rather than a ratio and a pool of points."""
+    def __init__(self, skills_dict):
+        self.skills = skills_dict
+        self.points = 0
+        self.categorised_skills = {}
+    def getSkills(self):
+        """return the skills dictionary"""
+        return self.skills
     
+    def allocateSkillPoints(self, skill_points):
+        """Allocate a number of points to be divided in some way between the skills. Here, skill_points is a list of tuples of (number of skills, point to add). For example: [(1,60), (2,40)] indicates one skill to be allocated 60 points and 2 to be allocated 40 points"""
+        if typecheck(skill_points):
+            self.points = skill_points
+        # zero all skills values before allocation
+        for skill in self.skills:
+            self.skills[skill] = 0
+        self.chooseSkills()
+
+    def chooseSkills(self):
+        """Decide how the skill points will be divided between the available skills"""
+        self.categoriseSkills()
+        # Add the points from the categories to the skills_dict
+        for category in self.categorised_skills.keys():
+            points = category[1]
+            skill_list = self.categorised_skills.get(category)
+            for skill in skill_list:
+                self.skills[skill] = points
+
+    def categoriseSkills(self):
+        """Put available skills into number of categories equal to length of points tuple list."""
+        # Allocate points based on the points tuples;
+        # the tuples become the category names.
+        # eg for (1,60):
+        # 1. pick 1 random skill from self.skills
+        # 2. add 60 points to it
+        skill_names = list(self.skills.keys())
+        shuffle(skill_names)
+        for category in self.points:
+            self.categorised_skills[category] = []
+            
+        for category in self.categorised_skills.keys():
+            for x in range(0, category[0]):
+                self.categorised_skills[category].append(skill_names.pop())
+        
+            
